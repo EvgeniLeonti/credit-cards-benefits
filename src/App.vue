f9a28b68bb0d54b6e8f3324a36f1331cf4ca1551
Evgeni Leonti <evgeni.leonti@unipaas.com>
2022-02-06 22:44:08 +0200

    sidebar added + mobile responsive version

--- src/App.vue
+++ src/App.vue
@@ -1,204 +1,225 @@
 <template>
   <v-app id="inspire">
-    <v-app-bar
+    <v-navigation-drawer
         app
-        dark
-        flat
+        right
+        v-model="drawer"
+        clipped
+        color="transparent"
+        width="300"
     >
-      <v-toolbar-title>הטבות כרטיסי אשראי</v-toolbar-title>
 
-      <v-spacer></v-spacer>
-      <v-responsive max-width="260">
+      <v-sheet
+          class="my-5 px-3 mx-3"
+          min-height="70vh"
+          rounded="lg"
+      >
+        <h3 class="mx-5 mt-5 pt-5">חיפוש הטבות</h3>
         <v-text-field
+            class="my-3"
             v-model="search"
             clearable
             flat
             dense
             solo-inverted
             hide-details
-            rounded
             prepend-inner-icon="mdi-magnify"
-            label="חיפוש"
+            label="טקסט חופשי"
         ></v-text-field>
-      </v-responsive>
-    </v-app-bar>
+        <v-divider />
 
+        <h3 class="mx-5 mt-5">כרטיסי אשראי</h3>
+        <v-list color="transparent">
+          <v-list-item-group
+              v-model="selectedProviders"
+              multiple
+          >
+            <v-list-item v-for="provider in providers" :key="`provider-${provider}`">
+              <template v-slot:default="{ active }">
+                <v-list-item-action>
+                  <v-checkbox :input-value="active"></v-checkbox>
+                </v-list-item-action>
+
+                <v-list-item-content>
+                  <v-list-item-title>{{ translateProvider(provider) }}</v-list-item-title>
+                </v-list-item-content>
+              </template>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+        <div style="text-align: center">
+          <v-btn text link @click="selectedProviders = Object.values(providers).map((val, index) => index)">בחר הכל</v-btn>
+          <v-btn text link @click="selectedProviders = []">אפס הכל</v-btn>
+        </div>
+
+        <v-divider />
+
+        <h3 class="mx-5 mt-5">קטגוריות</h3>
+        <v-list color="transparent">
+          <v-list-item-group
+              v-model="selectedCategories"
+              multiple
+              active-class=""
+          >
+            <v-list-item v-for="category in categories" :key="`category-${category}`">
+              <template v-slot:default="{ active }">
+                <v-list-item-action>
+                  <v-checkbox :input-value="active"></v-checkbox>
+                </v-list-item-action>
+
+                <v-list-item-content>
+                  <v-list-item-title >{{ translateCategory(category) }}</v-list-item-title>
+                </v-list-item-content>
+              </template>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+        <div style="text-align: center">
+          <v-btn text link @click="selectedCategories = Object.values(categories).map((val, index) => index)">בחר הכל</v-btn>
+          <v-btn text link @click="selectedCategories = []">אפס הכל</v-btn>
+        </div>
+      </v-sheet>
+
+
+    </v-navigation-drawer>
+    <v-app-bar
+        app
+        dark
+        flat
+        clipped-right
+    >
+      <v-toolbar-title>הטבות ומבצעים בכרטיסי אשראי</v-toolbar-title>
+
+
+
+      <v-spacer></v-spacer>
+
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+
+    </v-app-bar>
     <v-main class="grey lighten-3">
       <v-container>
-        <v-row>
-          <v-col cols="3">
-            <v-sheet rounded="lg" class="my-2">
-              <br />
-              <h2 class="mx-5">כרטיסי אשראי</h2>
-              <v-list color="transparent">
-                <v-list-item-group
-                    v-model="selectedProviders"
-                    multiple
+        <v-sheet
+            class="my-2 px-3 py-3"
+            min-height="70vh"
+            rounded="lg"
+        >
+          <v-alert color="success" v-if="search">
+            <div style="color: white">מציג תוצאות חיפות עבור <strong>{{search}}</strong></div>
+          </v-alert>
+          <v-data-iterator
+              :items="items"
+              :items-per-page.sync="itemsPerPage"
+              :page.sync="page"
+              :sort-by="sortBy.toLowerCase()"
+              :sort-desc="sortDesc"
+              hide-default-footer
+              no-data-text="אין תוצאות"
+              no-results-text="אין תוצאות"
+          >
+
+
+            <template v-slot:default="props">
+              <v-row>
+                <v-col
+                    v-for="item in props.items"
+                    :key="item.url"
+                    cols="12"
+                    sm="6"
+                    md="4"
+                    lg="3"
                 >
-                  <v-list-item v-for="provider in providers" :key="`provider-${provider}`">
-                    <template v-slot:default="{ active }">
-                      <v-list-item-action>
-                        <v-checkbox :input-value="active"></v-checkbox>
-                      </v-list-item-action>
+                  <v-card :href="item.url" target="_blank">
+                    <v-img
+                        height="250"
+                        :src="item.image"
+                    >
 
-                      <v-list-item-content>
-                        <v-list-item-title>{{ provider }}</v-list-item-title>
-                      </v-list-item-content>
-                    </template>
-                  </v-list-item>
-                </v-list-item-group>
-              </v-list>
-              <v-btn text link @click="selectedProviders = Object.values(providers).map((val, index) => index)">בחר הכל</v-btn>
-              <v-btn text link @click="selectedProviders = []">אפס הכל</v-btn>
-              <br />
-              <br />
-              <v-divider />
-              <br />
+                      <v-chip
+                          class="ma-2"
+                          :color="getProviderColor(item.provider)"
+                          text-color="white"
+                          small
+                      >
+                        {{ translateProvider(item.provider) }}
+                      </v-chip>
 
-              <h2 class="mx-5">קטגוריות</h2>
-              <v-list color="transparent">
-                <v-list-item-group
-                    v-model="selectedCategories"
-                    multiple
-                    active-class=""
-                >
-                  <v-list-item v-for="category in categories" :key="`category-${category}`">
-                    <template v-slot:default="{ active }">
-                      <v-list-item-action>
-                        <v-checkbox :input-value="active"></v-checkbox>
-                      </v-list-item-action>
+                      <v-chip
+                          class="ma-2"
+                          color="yellow"
+                          text-color="black"
+                          small
+                      >
+                        {{ translateCategory(item.category) }}
+                      </v-chip>
+                    </v-img>
 
-                      <v-list-item-content>
-                        <v-list-item-title >{{ category }}</v-list-item-title>
-                      </v-list-item-content>
-                    </template>
-                  </v-list-item>
-                </v-list-item-group>
-              </v-list>
-              <v-btn text link @click="selectedCategories = Object.values(categories).map((val, index) => index)">בחר הכל</v-btn>
-              <v-btn text link @click="selectedCategories = []">אפס הכל</v-btn>
-              <br />
-              <br />
-            </v-sheet>
-          </v-col>
+                    <v-card-title class="subheading font-weight-bold">
+                      {{ item.title }}
+                    </v-card-title>
 
-          <v-col cols="9">
-            <v-sheet
-                class="my-5 px-3"
-                min-height="70vh"
-                rounded="lg"
-            >
-              <v-alert v-if="search">מציג תוצאות חיפות עבור <strong>{{search}}</strong></v-alert>
-              <v-data-iterator
-                  :items="items"
-                  :items-per-page.sync="itemsPerPage"
-                  :page.sync="page"
-                  :search="search"
-                  :sort-by="sortBy.toLowerCase()"
-                  :sort-desc="sortDesc"
-                  hide-default-footer
+                  </v-card>
+                </v-col>
+              </v-row>
+            </template>
+
+            <template v-slot:footer>
+              <v-row
+                  v-if="items && items.length > 0"
+                  class="px-5 py-5"
+                  align="center"
+                  justify="center"
               >
-
-
-                <template v-slot:default="props">
-                  <v-row>
-                    <v-col
-                        v-for="item in props.items"
-                        :key="item.url"
-                        cols="12"
-                        sm="6"
-                        md="4"
-                        lg="3"
-                    >
-                      <v-card :href="item.url" target="_blank">
-                        <v-img
-                            height="250"
-                            :src="item.image"
-                        >
-
-                          <v-chip
-                              class="ma-2"
-                              :color="getProviderColor(item.provider)"
-                              text-color="white"
-                              small
-                          >
-                            {{ item.provider }}
-                          </v-chip>
-                        </v-img>
-
-                        <v-card-title class="subheading font-weight-bold">
-                          {{ item.title }}
-                        </v-card-title>
-
-                      </v-card>
-                    </v-col>
-                  </v-row>
-                </template>
-
-                <template v-slot:footer>
-                  <v-row
-                      class="mt-2 mb-5"
-                      align="center"
-                      justify="center"
-                  >
-                    <span class="grey--text">מס' פריטים בעמוד</span>
-                    <v-menu offset-y>
-                      <template v-slot:activator="{ on, attrs }">
-                        <v-btn
-                            dark
-                            text
-                            color="primary"
-                            class="ml-2"
-                            v-bind="attrs"
-                            v-on="on"
-                        >
-                          {{ itemsPerPage }}
-                          <v-icon>mdi-chevron-down</v-icon>
-                        </v-btn>
-                      </template>
-                      <v-list>
-                        <v-list-item
-                            v-for="(number, index) in itemsPerPageArray"
-                            :key="index"
-                            @click="updateItemsPerPage(number)"
-                        >
-                          <v-list-item-title>{{ number }}</v-list-item-title>
-                        </v-list-item>
-                      </v-list>
-                    </v-menu>
-
-                    <v-spacer></v-spacer>
-
-                    <span
-                        class="mr-4
-            grey--text"
-                    >
-            עמוד {{ page }} מתוך {{ numberOfPages }}
-          </span>
+                <span class="grey--text">מס' פריטים בעמוד</span>
+                <v-menu offset-y>
+                  <template v-slot:activator="{ on, attrs }">
                     <v-btn
-                        fab
                         dark
-                        color="blue darken-3"
-                        class="mr-1"
-                        @click="formerPage"
+                        text
+                        color="primary"
+                        class="ml-2"
+                        v-bind="attrs"
+                        v-on="on"
                     >
-                      <v-icon>mdi-chevron-right</v-icon>
+                      {{ itemsPerPage }}
+                      <v-icon>mdi-chevron-down</v-icon>
                     </v-btn>
-                    <v-btn
-                        fab
-                        dark
-                        color="blue darken-3"
-                        class="ml-1"
-                        @click="nextPage"
+                  </template>
+                  <v-list>
+                    <v-list-item
+                        v-for="(number, index) in itemsPerPageArray"
+                        :key="index"
+                        @click="updateItemsPerPage(number)"
                     >
-                      <v-icon>mdi-chevron-left</v-icon>
-                    </v-btn>
-                  </v-row>
-                </template>
-              </v-data-iterator>
-            </v-sheet>
-          </v-col>
-        </v-row>
+                      <v-list-item-title>{{ number }}</v-list-item-title>
+                    </v-list-item>
+                  </v-list>
+                </v-menu>
+
+                <v-spacer></v-spacer>
+
+                <span class="ml-4 grey--text">עמוד {{ page }} מתוך {{ numberOfPages }}</span>
+                <v-btn
+                    fab
+                    dark
+                    class="ml-1"
+                    @click="formerPage"
+                >
+                  <v-icon>mdi-chevron-right</v-icon>
+                </v-btn>
+                <v-btn
+                    fab
+                    dark
+                    class="mr-1"
+                    @click="nextPage"
+                >
+                  <v-icon>mdi-chevron-left</v-icon>
+                </v-btn>
+              </v-row>
+            </template>
+          </v-data-iterator>
+        </v-sheet>
+
       </v-container>
     </v-main>
   </v-app>
@@ -228,6 +249,21 @@ enum CATEGORY {
 export default Vue.extend({
   name: 'App',
 
+  beforeDestroy () {
+    if (typeof window === 'undefined') return
+
+    window.removeEventListener('resize', this.onResize, { passive: true })
+  },
+
+  mounted () {
+    this.onResize()
+
+    window.addEventListener('resize', this.onResize, { passive: true })
+
+    this.drawer = !this.isMobile
+  },
+
+
   data () {
     // shuffle array
     for (let i = data.length - 1; i > 0; i--) {
@@ -237,6 +273,9 @@ export default Vue.extend({
 
 
     return {
+      drawer: false,
+      group: null,
+      isMobile: false,
       itemsPerPageArray: [24, 48, 72],
       search: '',
       filter: {},
@@ -274,11 +313,26 @@ export default Vue.extend({
     },
   },
   methods: {
+    onResize () {
+      (this as any).isMobile = window.innerWidth < 800
+    },
     nextPage () {
       if (this.page + 1 <= this.numberOfPages) this.page += 1
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+      });
+
     },
     formerPage () {
       if (this.page - 1 >= 1) this.page -= 1
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+      });
+
     },
     updateItemsPerPage (number) {
       this.itemsPerPage = number
@@ -299,29 +353,69 @@ export default Vue.extend({
       const selectedCategories = this.selectedCategories.map(selectedIndex => Object.values(CATEGORY)[selectedIndex]);
       const selectedProviders = this.selectedProviders.map(selectedIndex => Object.values(PROVIDER)[selectedIndex]);
       this.items = this.originalItems.filter(item => selectedProviders.find(provider => provider === item.provider));
-      this.items = this.items.filter(item => selectedCategories.find(category => category === item.category))
+      this.items = this.items.filter(item => selectedCategories.find(category => category === item.category));
+      if (this.search && this.search.length > 0) {
+        this.items = this.items.filter(item => item.title.indexOf(this.search) > -1);
+      }
+    },
+    translateProvider(provider) {
+      switch (provider) {
+        case PROVIDER.CAL:
+          return 'כאל'
+        case PROVIDER.ISRACARD:
+          return 'ישראכארט'
+        case PROVIDER.MAX:
+          return 'מקס'
+      }
+    },
+    translateCategory(category) {
+      switch (category) {
+        case CATEGORY.CINEMA:
+          return 'קולנוע';
+        case CATEGORY.FOOD:
+          return 'אוכל';
+        case CATEGORY.ATTRACTIONS:
+          return 'ארקציות';
+        case CATEGORY.MISC:
+          return 'שונות';
+        case CATEGORY.TRAVELS:
+          return 'טיולים וחופשות';
+        case CATEGORY.ENTERTAINMENT:
+          return 'בידור ופנאי';
+        case CATEGORY.PARENTS:
+          return 'ילדים';
+        case CATEGORY.STANDUP:
+          return 'סטנד אפ';
+      }
     },
   },
   watch: {
     selectedCategories: {
-      handler(selectedCategoriesIndexes, oldValue) {
+      handler(newValue, oldValue) {
         this.applyFilters()
       },
       immediate: true
     },
     selectedProviders: {
-      handler(selectedProvidersIndexes, oldValue) {
+      handler(newValue, oldValue) {
         this.applyFilters()
       },
       immediate: true
-    }
+    },
+    search: {
+      handler(newValue, oldValue) {
+        this.applyFilters()
+      },
+      immediate: true
+    },
+    group () {
+      this.drawer = false
+    },
   },
 });
 </script>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css2?family=Assistant&display=swap');
-
 html, body {
   font-family: 'Assistant', sans-serif;
 }
